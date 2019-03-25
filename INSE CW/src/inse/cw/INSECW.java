9e35b363655176fb6237f12d2950042fccf70957
Vihan99 <36368384+Vihan99@users.noreply.github.com>
2019-03-25 11:49:46 +0000

    Add files via upload

---
+++ INSE CW/src/inse/cw/INSECW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inse.cw;
+
+/**
+ *
+ * @author vihan
+ */
+public class INSECW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
